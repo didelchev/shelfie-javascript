76817da7afdb5b21d682be9345084dee848349ea
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-03 13:21:24 +0300

    Changed server dirpath

--- shelfie-app-v2/server/server.js
+++ shelfie-app-v2/server/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 
+const path = require('path')
+
 const app = express()
 
 const port = 3000
 
+
 app.get(/(.*)/, (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  
 })
 
 app.listen(port, () => {
