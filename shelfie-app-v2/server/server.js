a8a8bc804f24e032480b2a3c5f9a758c13e9fafc
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-03 17:15:24 +0300

    Updated folder structure

--- shelfie-app-v2/server/server.js
+++ shelfie-app-v2/server/server.js
@@ -1,18 +1,29 @@
-const express = require('express')
+import express from 'express'
 
-const path = require('path')
+import path from 'path'
+
+import { dirname } from 'path';
+
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+
+
 
 const app = express()
 
-const port = 3000
-
+app.use(express.static(path.join(__dirname, '..', '/public')))
 
 app.get(/(.*)/, (req, res) => {
   
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
   
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:3000 ...`)
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000...`)
 })
