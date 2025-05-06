513df25f5fb0d31e45b464cf522518e7f5ee3227
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-06 13:31:29 +0300

    Updated server.js

--- shelfie-app-v2/server/server.js
+++ shelfie-app-v2/server/server.js
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '..', '/public')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get(/(.*)/, (req, res) => {
   
