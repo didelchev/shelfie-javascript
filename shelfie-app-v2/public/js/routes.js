837718a3e5b051ac882e0d1ac24ec137dc918ee9
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-07 21:53:11 +0300

    Removed  old routes

--- shelfie-app-v2/public/js/routes.js
+++ shelfie-app-v2/public/js/routes.js
@@ -1,19 +1,3 @@
-import { bookComponent } from "./components/bookComponent.js";
-import { catalogComponent } from "./components/catalogComponent.js";
-import { homeComponent } from "./components/homeComponent.js";
-import { loginComponent } from "./components/loginComponent.js";
-import { registerComponent } from "./components/registerComponent.js";
-
-
-
-// const routes = [
-//   { path: '/', component: homeComponent },
-//   { path: '/catalog', component: catalogComponent},
-//   { path: '/login', component: loginComponent},
-//   { path: '/register', component: registerComponent},
-//   { path: '/book-details', component: bookComponent },
-
-// ]
 const routes = []
 
 
