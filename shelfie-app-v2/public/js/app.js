b136f570ca4ef0414c4ca4552b3ff1d93ade3cc0
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-07 21:53:22 +0300

    Added new routes

--- shelfie-app-v2/public/js/app.js
+++ shelfie-app-v2/public/js/app.js
@@ -1,8 +1,26 @@
+import { bookComponent } from "./components/bookComponent.js";
+import { catalogComponent } from "./components/catalogComponent.js";
 import { homeComponent } from "./components/homeComponent.js";
+import { loginComponent } from "./components/loginComponent.js";
+import { registerComponent } from "./components/registerComponent.js";
 import { startRouter } from "./routes.js";
 import { useRoutes } from "./routes.js";
+
 useRoutes('/', homeComponent)
+useRoutes('/catalog', catalogComponent)
+useRoutes('/login', loginComponent)
+useRoutes('/register', registerComponent)
+useRoutes('/book-details', bookComponent)
 
 
 startRouter()
 
+
+// const routes = [
+//   { path: '/', component: homeComponent },
+//   { path: '/catalog', component: catalogComponent},
+//   { path: '/login', component: loginComponent},
+//   { path: '/register', component: registerComponent},
+//   { path: '/book-details', component: bookComponent },
+
+// ]
