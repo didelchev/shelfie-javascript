6c7d5aefb6412e4d9e447f7ca40d1caa6e5ef9ef
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-08 04:29:23 +0300

    Added express router

--- shelfie-app-v3/server/src/index.js
+++ shelfie-app-v3/server/src/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import routes from './routes.js';
 
 const app = express()
 
 
-app.get("/", (req,res) => {
+app.use(routes)
+
+
+routes.get("/", (req,res) => {
     res.send('It works')
 })
 
 
 app.listen(5000, () => {
-    console.log('Server is working on http://localhost:5000');
+    console.log('Server is working on http://localhost:5000...');
     
 })
