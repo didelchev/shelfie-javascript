ff9f5785b90e24637a6742de2b2d18c8f8b34ab1
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-08 04:20:32 +0300

    Initialized express server

---
+++ shelfie-app-v3/server/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express()
+
+
+app.get("/", (req,res) => {
+    res.send('It works')
+})
+
+
+app.listen(5000, () => {
+    console.log('Server is working on http://localhost:5000');
+    
+})
