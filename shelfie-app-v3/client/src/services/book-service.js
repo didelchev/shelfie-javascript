1e48fef48131b851650ecbdeb5893e4435508248
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-19 23:51:49 +0300

    Updated book service

--- shelfie-app-v3/client/src/services/book-service.js
+++ shelfie-app-v3/client/src/services/book-service.js
@@ -2,9 +2,9 @@ import { get } from "./requester.js"
 
 const baseURL = "http://localhost:5000/catalog";
 
-const getAll = () => get(baseURL)
+export const getAll = () => get(baseURL)
 
-const getOne = (bookId) => get(baseURL).then(data => data.find(book => book.id === bookId))
+export const getOne = (bookId) => get(baseURL).then(data => data.find(book => book.id === bookId))
 
 
 // const getOne = (movieId) => get(`${serverURL}/${movieId}`).then(data => console.log(data)
