cbc576e9c2bf6c35145c48148ee68054d70ac739
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-23 22:47:48 +0300

    Added single book request

--- shelfie-app-v3/client/src/services/book-service.js
+++ shelfie-app-v3/client/src/services/book-service.js
@@ -4,9 +4,5 @@ const baseURL = "http://localhost:5000/catalog";
 
 export const getAll = () => get(baseURL)
 
-export const getOne = (bookId) => get(baseURL).then(data => data.find(book => book.id === bookId))
-
-
-// const getOne = (movieId) => get(`${serverURL}/${movieId}`).then(data => console.log(data)
-// )
+export const getOne = (bookId) => get(`${baseURL}/${bookId}`)
 
