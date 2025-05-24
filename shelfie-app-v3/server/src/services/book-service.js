09101d9eeca51f005c812f78406591266c9e945f
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-24 18:52:06 +0300

    Update

---
+++ shelfie-app-v3/server/src/services/book-service.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+const apiKey = process.env.API_KEY
+const url = 
+
+
+const getAll = () => {
+
+}
--- shelfie-app-v3/server/src/test.js
+++ shelfie-app-v3/server/src/test.js
@@ -1,5 +1,4 @@
-const apiKey = process.env.API_KEY
-import 'dotenv/config'
+
 
 function loadGenre(genre) {
     const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=1&key=${apiKey}`;
