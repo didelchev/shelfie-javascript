75067a0109df25346ecb19b72b47d54f04f3c701
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-24 03:40:32 +0300

    Update

--- shelfie-app-v3/server/src/test.js
+++
@@ -1,8 +0,0 @@
-
-
-fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=20&key=AIzaSyC9UVLcCJ3Z1RgqcwLcO_oDcP4Mk5F4W9Y`)
-    .then(res => res.json())
-    .then(data => data.items.forEach(book => {
-        console.log(book.volumeInfo.title);
-        
-    }) )
