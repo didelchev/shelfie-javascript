307d3323bbcd61b15150809a7694670871f76009
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-25 16:12:58 +0300

    Created Book model

---
+++ shelfie-app-v3/server/src/models/Book.js
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+const bookSchema = new Schema({
+    title: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    pages: {
+        type: Number
+    },
+    description: {
+        type: String
+    },
+    image: { 
+        type: String
+    },
+    genre: {
+        type: String
+    }
+})
+
+const Book = model("Book", bookSchema )
+
+export default Book
