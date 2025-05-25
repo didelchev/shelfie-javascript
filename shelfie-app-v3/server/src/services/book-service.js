b1f3c4ec1730c6a18bba4ab5a50f75c8ceda4653
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-25 12:28:17 +0300

    Added getAllByGenre function

--- shelfie-app-v3/server/src/services/book-service.js
+++ shelfie-app-v3/server/src/services/book-service.js
@@ -1,9 +1,22 @@
 import 'dotenv/config'
+import { get } from './requester.js';
 
 const apiKey = process.env.API_KEY
-const url = 
 
 
-const getAll = () => {
+const getAllByGenre = (genre) => {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=5&key=${apiKey}`;
 
+    get(url).then(data => data.items.forEach(book => console.log(book.volumeInfo.title)))
+        
+}
+
+
+const getOne = (bookId) => {
+    
+}
+
+
+export {
+    getAllByGenre
 }
--- shelfie-app-v3/server/src/test.js
+++ shelfie-app-v3/server/src/test.js
@@ -1,13 +1,18 @@
 
 
-function loadGenre(genre) {
-    const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=1&key=${apiKey}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => data.items.forEach(item => console.log(item.volumeInfo.pageCount)
-        ))
+// function loadGenre(genre) {
+//     const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=1&key=${apiKey}`;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => data.items.forEach(item => console.log(item.volumeInfo.pageCount)
+//         ))
+
+import { getAllByGenre } from "./services/book-service.js";
+
         
-  }
+//   }
   
 
-  loadGenre('fiction')
+//   loadGenre('fiction')
+
+getAllByGenre('fiction')
