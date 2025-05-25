bb1f284f167b1535790f636491e31510b8735da2
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-25 15:39:28 +0300

    Added mongodb

--- shelfie-app-v3/client/src/components/catalog-component.js
+++ shelfie-app-v3/client/src/components/catalog-component.js
@@ -7,20 +7,25 @@ import { singleBookComponent } from "./book-template.js"
 export const catalogComponent = () => {
   document.title = 'Explore Books'
   
-  getAll()
-    .then(books => {
-      const allBooksHTML = books.map(book => singleBookComponent(book)).join('')
-      render(`
-    <main class="book-catalog">
-      <h1>Explore books</h1>
-      <div class="book-catalog-grid">
-        ${(allBooksHTML)}
-      </div>
-    </main> 
-        `)
-    })
-    .catch(error => console.log(error)
-    )
+ getAll()
+  .then(data => console.log(data)
+  )
+  
+
+    
+    // .then(books => {
+    //   const allBooksHTML = books.map(book => singleBookComponent(book)).join('')
+    //   render(`
+    // <main class="book-catalog">
+    //   <h1>Explore books</h1>
+    //   <div class="book-catalog-grid">
+    //     ${(allBooksHTML)}
+    //   </div>
+    // </main> 
+    //     `)
+    // })
+    // .catch(error => console.log(error)
+    // )
 
     
 Navigate()
