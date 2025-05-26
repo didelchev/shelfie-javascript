875d68c0f7242ce79053a82fe7421109f42feb1d
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-26 14:38:06 +0300

    Fixed book template image tag

--- shelfie-app-v3/client/src/components/book-template.js
+++ shelfie-app-v3/client/src/components/book-template.js
@@ -2,7 +2,7 @@ export const singleBookComponent = (book) => {
 return`
     <div class='book'>
         <a class="link" href="/catalog/${book.id}">
-        <img src="${book.image} alt="${book.title}" />
+        <img src="${book.image}" alt="${book.title}" />
         <h4>${book.title}</h4>
         <p>${book.author}</p>
         </a>
