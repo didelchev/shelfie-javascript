7bb4d5d810031f2d5bd9b0ab92e7fe22732e812b
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-26 17:05:28 +0300

    Created getOne function

--- shelfie-app-v3/server/src/services/book-service.js
+++ shelfie-app-v3/server/src/services/book-service.js
@@ -1,13 +1,14 @@
 import Book from "../models/Book.js";
 
-
-
 const getAll = () => Book.find().lean()
+
+const getOne = (bookId) => Book.findById(bookId)
 
 
 
 
 
 export {
-    getAll
+    getAll,
+    getOne
 }
