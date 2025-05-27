64dcc42e7b583c9fb89df5f555019b96467daac0
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-28 02:25:57 +0300

    Fixed href

--- shelfie-app-v3/client/src/components/book-template.js
+++ shelfie-app-v3/client/src/components/book-template.js
@@ -1,7 +1,7 @@
 export const bookTemplateComponent = (book) => {
 return`
     <div class='book'>
-        <a class="link" href="./catalog/${book._id}">
+        <a class="link" href="/catalog/${book._id}">
         <img src="${book.image}" alt="${book.title}" />
         <h4>${book.title}</h4>
         <p>${book.author}</p>
