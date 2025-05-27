2158023cb1196367150f907d2bdf8416ffb61db8
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-28 02:26:50 +0300

    Added authController

---
+++ shelfie-app-v3/server/src/controllers/auth-controller.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+
+
+
+const authController = Router()
+
+
+authController.get('/', (req,res) => {
+    res.send('It works')
+})
+
+
+
+
+export default authController
