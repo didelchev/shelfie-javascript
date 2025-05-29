4b8c3a7c98298c7a644b841a778fd9e69477178e
Daniel Delchev <daniel.delchev@yahoo.com>
2025-05-29 13:58:37 +0300

    Updated authController

--- shelfie-app-v3/server/src/controllers/auth-controller.js
+++ shelfie-app-v3/server/src/controllers/auth-controller.js
@@ -6,11 +6,15 @@ import { login, register } from "../services/auth-service.js";
 const authController = Router()
 
 
-authController.get('/', (req,res) => {
+authController.get('/register', (req,res) => {
     register('peter.ivanov', 'ivanov.peter@abv.bg', '4312')
         .then(() => console.log('Succes'))
-        .catch((err) => console.log('User existststs beee'))
+        .catch((err) => console.log('User already exists'))
 
+})
+
+authController.get('/login', (req,res) => {
+    login()
 })
 
 
