bfaec507971f7bb21de8ec8b98b6b5b2c4833a6b
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-01 04:29:01 +0300

    Updated register service to send username

--- shelfie-app-v3/client/src/services/auth-service.js
+++ shelfie-app-v3/client/src/services/auth-service.js
@@ -3,10 +3,12 @@ import { post } from "./requester.js"
 const baseURL = 'http://localhost:5000/auth'
 
 
-export const register = (email, password) => {
+export const register = (email, username, password, rePassword) => {
     const data = {
         email,
-        password
+        username,
+        password,
+        rePassword
     }
 
     return post(baseURL, data)
