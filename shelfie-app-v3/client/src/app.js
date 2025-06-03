6e00b5d4651068b083dfdc9fa520d191ba0e8c5c
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-03 18:11:58 +0300

    Added logout component

--- shelfie-app-v3/client/src/app.js
+++ shelfie-app-v3/client/src/app.js
@@ -2,6 +2,7 @@ import { bookComponent } from "./components/book-component.js";
 import { catalogComponent } from "./components/catalog-component.js";
 import { homeComponent } from "./components/home-component.js";
 import { loginComponent } from "./components/login-component.js";
+import { logoutComponent } from "./components/logout-component.js";
 import { registerComponent } from "./components/register-component.js";
 import { startRouter } from "./routes.js";
 import { useRoutes } from "./routes.js";
@@ -11,7 +12,7 @@ useRoutes('/login', loginComponent)
 useRoutes('/register', registerComponent)
 useRoutes('/catalog', catalogComponent)
 useRoutes('/catalog/:bookId', bookComponent)
-useRoutes('/logout', bookComponent)
+useRoutes('/logout', logoutComponent)
 
 
 startRouter()
