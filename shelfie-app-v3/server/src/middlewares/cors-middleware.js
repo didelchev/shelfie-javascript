944e4c6b550033768f7aff8ee730f1faea5fb7bd
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-05 02:51:15 +0300

    Updated cors middleware

--- shelfie-app-v3/server/src/middlewares/cors-middleware.js
+++ shelfie-app-v3/server/src/middlewares/cors-middleware.js
@@ -1,6 +1,6 @@
 export const corsMiddleware = (req,res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Or specify domains
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
     next()
 }
