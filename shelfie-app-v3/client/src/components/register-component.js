0a142384caabbd2f3e213598f206428abe1a6be0
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-06 16:27:31 +0300

    Updated register view to use lit html

--- shelfie-app-v3/client/src/components/register-component.js
+++ shelfie-app-v3/client/src/components/register-component.js
@@ -1,59 +1,52 @@
-// import { Navigate, Redirect } from "../routes.js";
-// import { register } from "../services/auth-service.js";
-// import { showMessage } from "../utils/notification.js";
-// import { render } from "../utils/render.js";
-// import { saveUserData } from "../utils/user-data.js";
+import { render,html } from "../lib.js"
+import { Navigate, Redirect } from "../routes.js";
+import { register } from "../services/auth-service.js";
+import { showMessage } from "../utils/notification.js";
+import { updateNav } from "../utils/update-nav.js";
+import { saveUserData } from "../utils/user-data.js";
 
-// export const registerComponent = () => {
-//   document.title = "Register Page";
+const registerTemplate = (registerHandler) => html`
+    <section class="register-container">
+      <form @submit=${registerHandler}  class="register-form">
+        <h2>Register</h2>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" />
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <label for="re-password">Repeat Password</label>
+        <input type="password" id="re-password" name="re-password" />
+        <button class ='submit' type="submit">Register</button>
+      </form>
+    </section>
+    <footer>
+      <div class="footer">
+        <p>&copy; 2025 Shelfie - All rights reserved</p>
+        <div class="footer-links">
+          <a href="/privacy-policy">Privacy Policy</a>
+          <a href="/terms-of-service">Terms of Service</a>
+        </div>
+      </div>
+    </footer>
+    `
 
-//   render(`
-//     <section class="register-container">
-//       <form class="register-form">
-//         <h2>Register</h2>
-//         <label for="email">Email</label>
-//         <input type="text" id="email" name="email" />
-//         <label for="username">Username</label>
-//         <input type="text" id="username" name="username" />
-//         <label for="password">Password</label>
-//         <input type="password" id="password" name="password" />
-//         <label for="re-password">Repeat Password</label>
-//         <input type="password" id="re-password" name="re-password" />
-//         <button class ='submit' type="submit">Register</button>
-//       </form>
-//     </section>
-//     <footer>
-//       <div class="footer">
-//         <p>&copy; 2025 Shelfie - All rights reserved</p>
-//         <div class="footer-links">
-//           <a href="/privacy-policy">Privacy Policy</a>
-//           <a href="/terms-of-service">Terms of Service</a>
-//         </div>
-//       </div>
-//     </footer>
-//     `);
 
-//   const form = document.querySelector(".register-form");
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(formData);
+export const showRegisterView = () => {
+    const registerHandler = (e) => {
+        e.preventDefault()
 
-//     const { email, username, password, ["re-password"]: rePassword } = data;
+        let { email, username, password, ["re-password"]: rePassword} = Object.fromEntries(new FormData(e.currentTarget))
 
-//     if (password !== rePassword) {
-//       showMessage("Passwords missmatch");
-//       return;
-//     }
+        register(email,username, password, rePassword)
+            .then(data => {
+                saveUserData(data)
+                showMessage('Register Successful')
+                updateNav()
+                Redirect('/')
+            })
+    }
 
-//     register(email, username, password, rePassword)
-//       .then((data) => {
-//         saveUserData(data)
-//         showMessage("Login Successful");
-//         Redirect("/");
-//       })
-//       .catch((err) => showMessage(err));
-//   });
-
-//   Navigate();
-// };
+    render(registerTemplate(registerHandler))
+    Navigate()
+}
