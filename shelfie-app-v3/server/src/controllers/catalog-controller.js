9a64dc6cab647533b05a63b40e47aa3cb2123197
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-09 02:05:21 +0300

    Updated catalog controller with new route

--- shelfie-app-v3/server/src/controllers/catalog-controller.js
+++ shelfie-app-v3/server/src/controllers/catalog-controller.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getAll, getOne } from "../services/book-service.js";
+import User from "../models/User.js";
+import { getUserById } from "../services/user-service.js";
 
 const catalogController = Router();
 
@@ -17,5 +19,27 @@ catalogController.get("/:bookId", (req, res) => {
     
 
 });
+
+catalogController.post("/:bookId", async (req, res) => {
+  let bookId = req.params.bookId
+  let userId = req.user._id
+  
+  try {
+  const user = await getUserById(userId)
+
+  if(!user.readList.includes(bookId)){
+      user.readList.push(bookId)
+      await user.save()
+      res.status(200).json({message: 'Successfully added book !'})
+  }
+    
+  } catch (error) {
+    res.json({message: 'Failed to add book :(' })
+    
+  }
+  
+
+
+})
 
 export default catalogController;
