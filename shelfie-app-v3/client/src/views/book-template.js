4b77513652414e1ee728c35451f0aa375a6a91c1
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-13 14:23:26 +0300

    Added anchor tag for links

--- shelfie-app-v3/client/src/views/book-template.js
+++ shelfie-app-v3/client/src/views/book-template.js
@@ -4,9 +4,9 @@ export const bookTemplate = (book) =>
 html`
     <div class='book'>
         <a class="link" href="/catalog/${book._id}">
-        <img src="${book.image}" alt="${book.title}" />
-        <h4>${book.title}</h4>
-        <p>${book.author}</p>
+            <img src="${book.image}" alt="${book.title}" />
+            <h4>${book.title}</h4>
+            <p>${book.author}</p>
         </a>
      </div>
     `
