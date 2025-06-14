9b2a442fb997985c6e835eafba0da2468d2c7154
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-14 20:31:39 +0300

    Added search functionality

--- shelfie-app-v3/client/src/views/catalog-view.js
+++ shelfie-app-v3/client/src/views/catalog-view.js
@@ -4,24 +4,45 @@ import { getAll } from "../services/book-service.js"
 import { bookTemplate } from "./book-template.js"
 
 
-const catalogTemplate = (books) => html`
+const catalogTemplate = (books, submitHandler, searchHandler) => html`
 <main class="book-catalog">
       <h1>Explore books</h1>
-      <form>
-        <input placeholder="Searth for a book..." type="text" name="text" class="input">
+      <form @submit=${submitHandler}>
+        <input @input = ${searchHandler} placeholder="Searth for a book..." type="text" name="text" class="input">
         <button class ='search-button'type='submit'>Search</button>
       </form>
       <div class="book-catalog-grid">
           ${books.map(book => bookTemplate(book))}
       </div>
     </main> 
+    
 `
 
-export const showCatalogView = () => {
-  document.title = 'Explore'
+// const searchInput = document.querySelector(".input")
+//    searchInput.addEventListener('input', (e) => {
+//     console.log(e)
+//    })
+
+const searchHandler = (e) => {
+  const query = e.currentTarget.value
   getAll()
     .then(books => {
-      render(catalogTemplate(books))
+      let filteredBooks = books.filter(book => {
+        return book.title.toLowerCase().includes(query)
+      })
+      render(catalogTemplate(filteredBooks, submitHandler, searchHandler))
+    })
+}
+
+const submitHandler = (e) => {
+  e.preventDefault()
+  console.log(e)
+}
+
+export const showCatalogView = () => {
+  getAll()
+    .then(books => {
+      render(catalogTemplate(books , submitHandler, searchHandler))
       Navigate()
     })
 }
