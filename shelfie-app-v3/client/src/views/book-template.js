86e65262de16c7a12c4a3f73664b59367bcf370d
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-15 12:31:07 +0300

    Update

--- shelfie-app-v3/client/src/views/book-template.js
+++ shelfie-app-v3/client/src/views/book-template.js
@@ -4,7 +4,7 @@ export const bookTemplate = (book) =>
 html`
     <div class='book'>
         <a class="link" href="/catalog/${book._id}">
-            <img src="${book.image}" alt="${book.title}" />
+            <img src="${book.image}" alt="${book.title}"/>
             <h4>${book.title}</h4>
             <p>${book.author}</p>
         </a>
