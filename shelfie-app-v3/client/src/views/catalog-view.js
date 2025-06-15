ff9ab419c55cf2203037c12ded6a812a087e8276
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-15 12:24:41 +0300

    Created spinner component

--- shelfie-app-v3/client/src/views/catalog-view.js
+++ shelfie-app-v3/client/src/views/catalog-view.js
@@ -2,20 +2,21 @@ import { Navigate } from "../routes.js"
 import { render, html } from "../lib.js"
 import { getAll } from "../services/book-service.js"
 import { bookTemplate } from "./book-template.js"
+import { spinnerTemplate } from "./spinner-template.js"
 
 
 
 let allBooks = []
 
-const catalogTemplate = (books, submitHandler, searchHandler) => html`
-<main class="book-catalog">
+const catalogTemplate = (books, submitHandler, searchHandler) =>  html`
+    <main class="book-catalog">
       <h1>Explore books</h1>
       <form @submit=${submitHandler} class = 'search-form'>
         <input @input = ${searchHandler} placeholder="Search for a book..." type="text" name="text" class="input">
         <button class ='search-button'type='submit'>Search</button>
       </form>
       <div class="book-catalog-grid">
-          ${books.map(book => bookTemplate(book))}
+          ${ books.map(book => bookTemplate(book))}
       </div>
     </main> 
     
@@ -38,6 +39,11 @@ const searchHandler = (e) => {
 
 
 export const showCatalogView = () => {
+
+  if(!allBooks.length) {
+    render(spinnerTemplate())
+
+  }
   getAll()
     .then(books => {
       allBooks = books
