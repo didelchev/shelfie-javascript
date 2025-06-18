de82dde2476e1487b57bd5fc082c06a28a08d36a
Daniel Delchev <daniel.delchev@yahoo.com>
2025-06-18 13:25:45 +0300

    Created addRating function

--- shelfie-app-v3/server/src/services/book-service.js
+++ shelfie-app-v3/server/src/services/book-service.js
@@ -46,6 +46,7 @@ const addToBookList = async (userId, bookId, bookList) => {
   }
 };
 
+
 export { 
  getAll,
  getOne,
--- shelfie-app-v3/server/src/services/review-service.js
+++ shelfie-app-v3/server/src/services/review-service.js
@@ -12,3 +12,26 @@ export const addReview = (bookId, userEmail, review) => {
    return Review.create({bookId, userEmail, review})
 }
 
+
+export const addRating = (bookId, userId, rating) => {
+    return Review.findById({bookId})
+      .then(book => {
+         book.ratings.details.push({userId, value: rating})
+         book.ratings.average = calculateAverage(book.ratings)
+      })
+      
+}
+
+
+
+const calculateAverage = ( ratings ) => {
+   if(!ratings.details.length){
+      return 0
+   }
+
+   const sum = ratings.details.reduce((total, rating) => {
+      return total += rating.value
+   }, 0)
+
+   return sum / ratings.details.length
+}
