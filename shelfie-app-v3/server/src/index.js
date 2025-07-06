d82c416edc75d261491a6d0a3f6cbb4130fdc646
Daniel Delchev <daniel.delchev@yahoo.com>
2025-07-06 22:01:04 +0300

    Clean up files for deployment

--- shelfie-app-v3/server/src/index.js
+++ shelfie-app-v3/server/src/index.js
@@ -7,6 +7,8 @@ import cors from 'cors'
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 app.use(cors());
 
 app.use(express.json());
@@ -23,7 +25,7 @@ mongoose.connect(dbUrl, { dbName: 'shelfie'})
 
 app.use(routes)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('Server is working on http://localhost:5000...');
     
 })
--- shelfie-app-v3/start-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start server
-gnome-terminal -- bash -c "cd server && npm run dev; exec bash"
-
-# Start client
-gnome-terminal -- bash -c "cd client && npm start; exec bash"
-
