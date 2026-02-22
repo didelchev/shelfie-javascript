f427ab6cfa655c82b4bcc674b9de02f0c26865b9
Daniel Delchev <daniel.delchev@yahoo.com>
2026-02-22 14:26:54 +0200

    add login as demo user endpoint

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -36,4 +36,19 @@ authController.post("/login", async (req,res) => {
 })
 
 
+authController.post("/login/demouser", async (req, res) => { 
+    const DEMO_EMAIL = "johndoe@gmail.com";
+    const DEMO_PASSWORD = "1234";
+
+    try{
+        const demoUser = await login(DEMO_EMAIL, DEMO_PASSWORD)
+
+        res.status(200).json(demoUser)
+    }catch(err) {
+        res.status(400).json({ message: err.message || "Login failed !"})
+    }
+
+})
+
+
 export default authController   
